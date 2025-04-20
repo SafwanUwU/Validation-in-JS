9bb1f62b8a5feb8e7e8600a47551daa70de68598
Safwan Ahmad <safwanahmad787@gmail.com>
2025-04-20 20:18:10 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const setScreenValue = async (value) => {
+  document.getElementById("result").value =
+    document.getElementById("result").value + value;
+};
+const clearScreen = async () => {
+  document.getElementById("result").value = "";
+};
